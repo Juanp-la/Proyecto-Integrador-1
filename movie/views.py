a11c920d3dcddd6365e9c1a50bb7912aaf8d235c
Juanp-la <juanpablo.la2397@gmail.com>
2026-07-29 15:49:27 -0500

    cambio de about/prueba

--- movie/views.py
+++ movie/views.py
@@ -5,8 +5,8 @@ def home(request):
     #return HttpResponse ('<h1>Welcome to the home page</h1>')
      #return render (request,'home.html')
      return render (request,'home.html',{'name':'Jp'})
+def about(request):
+    return render(request, "about.html")
+        
 
 # Create your views here.
-def about(request):
-     return HttpResponse('<h1>Class P1</h1>')
-    
