b475b5e5a40d131381eb0b4f8b7f104b27b974a7
Juanp-la <juanpablo.la2397@gmail.com>
2026-07-29 17:18:37 -0500

    Busqueda de peliculas

--- movie/models.py
+++ movie/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 # Create your models here.
-class Moviem(models.Model):
+class Movie(models.Model):
     title=models.CharField(max_length=100)
     description=models.CharField(max_length=250)
-    image=models.ImageField(upload_to='movies/images/')
+    image=models.ImageField(upload_to='movie/images/')
     url=models.URLField(blank=True)
