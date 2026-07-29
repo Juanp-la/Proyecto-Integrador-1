29758edc7026d3576223d9b74f806de302609860
Juanp-la <juanpablo.la2397@gmail.com>
2026-07-29 17:56:08 -0500

    Bootstrap+organizacion

--- movie/models.py
+++ movie/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Movie(models.Model):
-    title=models.CharField(max_length=100)
-    description=models.CharField(max_length=250)
-    image=models.ImageField(upload_to='movie/images/')
-    url=models.URLField(blank=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    details = models.CharField(max_length=200, blank=True, null=True, help_text="e.g. Family/Fantasy · 2001 · 2h 32m")
+    image = models.ImageField(upload_to='movies/images', blank=True, null=True)
+    url = models.URLField(blank=True, null=True)
