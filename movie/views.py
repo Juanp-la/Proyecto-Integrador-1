d9f46475a5109c2b821ddd6b0d1b07fed27d50ae
Juanp-la <juanpablo.la2397@gmail.com>
2026-07-29 15:32:24 -0500

    Primer commit

---
+++ movie/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse 
+
+def home(request):
+    #return HttpResponse ('<h1>Welcome to the home page</h1>')
+     #return render (request,'home.html')
+     return render (request,'home.html',{'name':'Jp'})
+
+# Create your views here.
+def about(request):
+     return HttpResponse('<h1>Class P1</h1>')
+    
